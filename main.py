69832764f7d86e86d5e3385d13c1ee3a365f7f1a
ricardont <rontiverosmarquez@gmail.com>
2020-01-22 13:30:04 -0700

    show json info in view

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
-from service import Data
+from flask import Flask, render_template, jsonify
+from service import Inegi
 app = Flask(__name__)
-data = Data()
 @app.route('/')
 def hello():
-    return  str(data.r[0])
-    #return render_template("home.html")
-@app.route('/profile/<name>')
+    return render_template("home.html")
+@app.route('/poblacion/')
+def poblacion():
+    data = Inegi()
+    return jsonify(data.poblacion) 
+@app.route('/profile/<name>')    
 def profile(name=None):
     return render_template("profile.html", name=name)
 if __name__ == '__main__':
--- service.py
+++ service.py
@@ -1,13 +1,13 @@
 import json 
 import requests
 
-class Data:
+class Inegi:
     def __init__(self):
         #Llamado al API
         url='https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000002/es/00000/false/BISE/2.0/6bb38c8c-0c38-64aa-3e42-38d896399bd1?type=json'
         response= requests.get(url)
         if response.status_code==200:
-            content= json.loads(response.content)
+            content= response.json()
             Series=content['Series'][0]['OBSERVATIONS']     
         #Obtención de la lista de observaciones 
         Observaciones=[]
@@ -16,4 +16,4 @@ class Data:
         sum=0.0
         for i in range(0,len(Observaciones)): sum=sum+Observaciones[i];  
         resultado=sum/len(Observaciones);
-        self.r = Observaciones
+        self.poblacion = Series
