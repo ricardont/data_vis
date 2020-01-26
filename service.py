b609f836ee57938262f9343de651306ed2e193be
ricardont <rontiverosmarquez@gmail.com>
2020-01-26 00:16:01 -0700

    added layout

--- service.py
+++ service.py
@@ -5,6 +5,7 @@ class Inegi:
     def __init__(self):
         #Llamado al API
         url='https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000002/es/00000/false/BISE/2.0/6bb38c8c-0c38-64aa-3e42-38d896399bd1?type=json'
+        # https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1006000039/es/15/false/BISE/2.0/6bb38c8c-0c38-64aa-3e42-38d896399bd1?type=json < accidentes viales
         response= requests.get(url)
         if response.status_code==200:
             content= response.json()
